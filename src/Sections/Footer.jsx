0035d66c6050d4dce8aa4496bc70945986eca979
Harsh Agarwal <76611932+Harsh-Agarwals@users.noreply.github.com>
2024-04-10 17:49:04 +0530

    Add files via upload

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <div  className='flex flex-1 flex-col gap-4 sm:flex-row justify-between py-6 px-8 bg-[#0D021B] text-sm text-slate-200 tracking-wide'>
-      <p>All Rights Reserved <span className=' text-amber-400'>@Tailum 2024</span></p>
-      <p>Made with ❤️ by <a href="www.fusionlabs.co.in" target='blank' className='tracking-wider'><span className='text-sky-400'>Fusion</span><span className='text-blue-600'>Labs</span></a></p>
+      <p>All Rights Reserved <span className='text-amber-400'>@Tailum 2024</span></p>
+      <p>Made with ❤️ by <a href="https://www.fusionlabs.co.in" target='_blank' className='tracking-wider'><span className='text-sky-400'>Fusion</span><span className='text-blue-600'>Labs</span></a></p>
     </div>
   )
 }
